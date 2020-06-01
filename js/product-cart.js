e09db1b737a87efb76e6e95064c822705e8df1d2
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-06-01 20:07:39 -0300

    adds a 'remove from cart' button on product page

--- js/product-cart.js
+++ js/product-cart.js
@@ -1,4 +1,5 @@
 let addButton;
+let removeButton;
 let viewButton;
 let product = {};
 
@@ -8,13 +9,26 @@ function addToCart() {
   productInCart();
 }
 
+function removeFromCart() {
+  cartLS.remove(product.id);
+  productOutOfCart();
+}
+
 function productInCart() {
   addButton.classList.add('product-cart-button-disable');
+  removeButton.classList.remove('product-cart-button-hide');
   viewButton.classList.remove('product-cart-link-disable');
+}
+
+function productOutOfCart() {
+  addButton.classList.remove('product-cart-button-disable');
+  removeButton.classList.add('product-cart-button-hide');
+  viewButton.classList.add('product-cart-link-disable');
 }
 
 window.addEventListener('load', function() {
   addButton = document.getElementById('product-add-button');
+  removeButton = document.getElementById('product-remove-button');
   viewButton = document.getElementById('product-view-cart');
 
   product.id = addButton.getAttribute('data-id');
@@ -24,4 +38,6 @@ window.addEventListener('load', function() {
 
   if (cartLS.exists(product.id)) productInCart();
   else addButton.onclick = addToCart;
+
+  removeButton.onclick = removeFromCart;
 });
